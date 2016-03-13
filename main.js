686c6606b3d3b6338678e936991d8b1f87611743
Mike Fink <mike.g.fink@gmail.com>
2016-03-12 20:45:24 -0800

    Initial commit.

---
+++ main.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         ReviewHub
+// @namespace
+// @version      0.1
+// @description  Modify GitHub pull requests to require approval
+// @author       Mike Fink
+// @match        https://github.com/*/*/pull/*/files
+// @require      http://code.jquery.com/jquery-2.2.1.min.js
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+// Your code here...
